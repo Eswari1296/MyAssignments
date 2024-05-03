1c2e120f30bb61c2a2045a7466ee54385aa3b296
Eswari1296 <168284473+Eswari1296@users.noreply.github.com>
2024-05-03 23:54:19 +0530

    Add files via upload
    week1day2 assignment

---
+++ assignments/week1day2/FindMissingElement.java
@@ -0,0 +1,15 @@
+package assignments.week1day2;
+
+import java.util.Arrays;
+
+public class FindMissingElement {
+public static void main(String[] args) {
+	int [] nums= {1,4,3,2,8,6,7};
+	Arrays.sort(nums);
+	for (int i = 0; i < nums.length;i++) {
+		 if (nums[i] != i + 1) {
+             System.out.println("Missing element: " + (i + 1));
+             break;
+	}
+}
+}}
